7773a7238aa55363138ed5ad2f91eb345263c838
Nikita Shinov <shinov_96@mail.ru>
2022-02-07 20:33:47 +0300

    scene delegate

--- FanChatApp/WelcomeViewController.swift
+++ FanChatApp/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  FanChatApp
 //
 //  Created by max on 02.02.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
