8ae32017723a730ffb0c7bd39b5b2c4af1e61630
Nikita Shinov <shinov_96@mail.ru>
2022-03-23 20:23:35 +0300

    implement sf safariservices

--- FanChatApp/FeedViewController.swift
+++ FanChatApp/FeedViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class FeedViewController: UITableViewController {
     
@@ -33,6 +34,38 @@ class FeedViewController: UITableViewController {
         
     }
     
+    let spinner = UIActivityIndicatorView(style: .medium)
+    
+    private func configureSpinnerView() {
+        
+        tableView.addSubview(spinner)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: self.tableView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: self.tableView.centerYAnchor),
+            spinner.heightAnchor.constraint(equalToConstant: 30),
+            spinner.widthAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func showSpinnerLoadingView(isShowing: Bool) {
+        if isShowing {
+            self.spinner.isHidden = false
+            spinner.startAnimating()
+        } else if spinner.isAnimating {
+            spinner.stopAnimating()
+            spinner.isHidden = true
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+           let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+           alert.addAction(UIAlertAction(title: "Ок", style: UIAlertAction.Style.default, handler: nil))
+           self.present(alert, animated: true, completion: nil)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel.numberOfArticles()
     }
@@ -47,5 +80,17 @@ class FeedViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         200
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = viewModel.news[indexPath.row]
+        
+        let config = SFSafariViewController.Configuration()
+        guard let url = URL(string: item.link) else { return }
+        let safariVC = SFSafariViewController(url: url, configuration: config)
+        safariVC.modalPresentationStyle = .automatic
+        present(safariVC, animated: true, completion: nil)
     }
 }
