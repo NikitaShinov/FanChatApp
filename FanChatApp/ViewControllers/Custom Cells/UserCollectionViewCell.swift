4e4057f5ed17a8b1ff7ea9daeb967c8aa5209fff
Nikita Shinov <shinov_96@mail.ru>
2022-05-11 20:56:15 +0300

    collection view test layout

--- FanChatApp/ViewControllers/Custom Cells/UserCollectionViewCell.swift
+++ FanChatApp/ViewControllers/Custom Cells/UserCollectionViewCell.swift
@@ -15,4 +15,49 @@ class UserCollectionViewCell: UICollectionViewCell {
             
         }
     }
+    
+    static let identifier = "UserCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemRed
+        contentView.addSubview(userImage)
+        contentView.addSubview(userName)
+        contentView.addSubview(userTeam)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var userImage: CustomImageView = {
+        let image = CustomImageView()
+        return image
+    }()
+    
+    lazy var userName: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var userTeam: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        userName.frame = CGRect(x: 5,
+                                y: contentView.frame.size.height-50,
+                                width: contentView.frame.size.width-10,
+                                height: 50)
+        
+        userImage.frame = CGRect(x: 5,
+                                 y: 0,
+                                 width: contentView.frame.size.width-10,
+                                 height: contentView.frame.size.height-50)
+        
+    }
 }
--- FanChatApp/ViewControllers/Tabbar VC/UsersViewController.swift
+++ FanChatApp/ViewControllers/Tabbar VC/UsersViewController.swift
@@ -7,12 +7,51 @@
 
 import UIKit
 
-class UsersViewController: UIViewController {
+class UsersViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    private var viewModel: UsersViewModelProtocol!
+    
+    var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
         title = "Users"
         view.backgroundColor = .blue
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.size.width / 3, height: view.frame.size.width / 3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else { return }
+        collectionView.register(UserCollectionViewCell.self,
+                                forCellWithReuseIdentifier: UserCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.frame = view.bounds
+        view.addSubview(collectionView)
+        viewModel = UsersViewModel()
+        viewModel.getUsers {
+            DispatchQueue.main.async {
+                
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 
 }
+
