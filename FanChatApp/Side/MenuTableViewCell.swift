80c106e6d56023440952e34fdd949632f0380994
Nikita Shinov <shinov_96@mail.ru>
2022-05-18 14:04:38 +0300

    side menu cell minor fixes

--- FanChatApp/Side/MenuTableViewCell.swift
+++ FanChatApp/Side/MenuTableViewCell.swift
@@ -28,6 +28,9 @@ class MenuTableViewCell: UITableViewCell {
         guard let viewModel = viewModel else { return }
         imageView?.image = UIImage(systemName: viewModel.imageName)?.withTintColor(UIColor.lightGreen(),
                                                                                    renderingMode: .alwaysOriginal)
+        imageView?.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 40, height: 30)
+        
+        textLabel?.anchor(top: topAnchor, left: imageView?.rightAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 100, height: 30)
         textLabel?.text = viewModel.menuOption.rawValue
         textLabel?.textColor = .white
         imageView?.tintColor = .white
